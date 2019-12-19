604a941772897fe5f1b032b18ac392e4c0d544b0
Tony <Tony.ZhiyuanXie@gmail.com>
2019-12-19 00:24:41 -0800

    Initial Commit

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Tony Xie on 12/19/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
